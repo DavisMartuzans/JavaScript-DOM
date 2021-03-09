de1fc11132adcb1121984ac1f345bcc855103699
Davis Martuzans <martuzans.davis@gmail.com>
2021-03-09 15:10:41 +0200

    Tests 2

--- app.js
+++ app.js
@@ -8,4 +8,7 @@ console.log(Array.isArray(Array.from(titles)));
 
 Array.from(titles).forEach(function(title){
   console.log(title);
+books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+  
 });
